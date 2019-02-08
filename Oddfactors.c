1248b06ec5afe64ab9fbcf19b4972ffc41875083
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-08 12:42:25 +0530

    Create Oddfactors.c

---
+++ Oddfactors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)  
+{
+int n,i;
+scanf("%d",&n); 
+for(i=1;i<n;i++) 
+{ 
+if(n%i==0) 
+printf("%d ",i);
+}
+for(i=n;i<=n;i++)
+{
+if(n%i==0)
+{
+if(n%2!=0)
+printf("%d",n);
+}
+}
+return 0;
+}
