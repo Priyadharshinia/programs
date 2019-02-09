bfb6c9c0d840ce6aac05cc3f54d3e51478199aab
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-10 01:44:39 +0530

    Create Numeric.c

---
+++ Numeric.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main(void) 
+{
+  char a[100];
+  int i,count=0,l; 
+scanf("%s",a);  
+l=strlen(a);
+for(i=0;a[i]!='\0';i++) 
+{ 
+   if(isdigit(a[i])||a[i]=='.') 
+   { 
+     count++;
+   }
+} 
+if(count==l) 
+{ 
+  printf("yes");
+} 
+else 
+{
+  printf("no");
+}
+	return 0;
+}
