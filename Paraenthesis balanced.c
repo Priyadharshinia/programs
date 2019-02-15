79e02aec0620d71de18c6ccfaa4bea5326cff687
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-15 21:35:45 +0530

    Create Paraenthesis balanced.c

---
+++ Paraenthesis balanced.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a[1000];
+	int i,c=0,k=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]=='(')
+		c++;
+		if(a[i]==')')
+		k++;
+	} 
+	if(c==k)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
