7614ea201c4b7d5fa2d419fb68106738f002ef5d
Priya dharshini A <46416623+Priyadharshinia@users.noreply.github.com>
2019-02-16 22:28:48 +0530

    Update Indexvalue.c

--- Indexvalue.c
+++ Indexvalue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	int a[100],i,n;
+	int a[100],i,n,c=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
@@ -11,6 +11,19 @@ int main(void)
 	{
 		if(i==a[i])
 		printf("%d ",a[i]);
+		if(i!=a[i])
+		{
+		c++;
+		}
+	}
+	if(c==n)
+	{
+		printf("-1");
 	}
 return 0;
 }
+
+	
+	
+	
+	
